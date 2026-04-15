753e9cc26555bbfe1a9f7e44d9bbba23f1184c71
lohith008-cloud <lohithgayam007@gmail.com>
2026-04-15 17:10:38 +0530

    fixed path isseus

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ app = FastAPI(
 
 app.include_router(router)
 
-# 🔥 FIXED TEMPLATE PATH
+# ✅ FIXED PATH
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 
